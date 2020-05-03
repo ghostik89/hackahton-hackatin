474e567a54a8683681ed09ed69025056064ea6fc
Lady_Proger <facktartar@mail.com>
2020-05-03 11:31:59 +0400

    add not found page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import history from './history'
 import {AuthContext} from './Context/auth'
 import {PrivateRouter} from "./Components/PrivateRoueter";
 import {HomePage} from "./Containers/HomePage";
+import {NotFoundPage} from "./Containers/NotFoundPage";
 
 function App() {
     const [authTokens, setAuthTokens] = useState({user:{}, token:""});
@@ -22,6 +23,7 @@ function App() {
                 <Switch>
                     <Route exact path={"/"} component={props => <LoginPage   {...props}/>}/>
                     <PrivateRouter exact path={"/home"} component={HomePage}/>
+                    <Route path="*" component={props => <NotFoundPage{...props}/>}/>
                 </Switch>
             </Router>
         </AuthContext.Provider>
---
+++ src/Containers/NotFoundPage.jsx
@@ -0,0 +1,8 @@
+import Typography from '@material-ui/core/Typography';
+import React from "react";
+
+export const NotFoundPage = ({history}) => {
+    return(
+        <Typography>404 page</Typography>
+    )
+}
